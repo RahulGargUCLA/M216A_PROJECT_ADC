eab6921ebd2150766831ce74caecf38a694c2cb8
NanoCAD Projects <nanocad.ucla@gmail.com>
2019-11-25 16:29:47 -0800

    Removes work directory from last push.

--- RTL/work/@_opt/voptt2an62
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity NLC_testbench_stud is
-end NLC_testbench_stud;
--- RTL/work/@n@l@c_6th_order_16ch_testbench/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity NLC_6th_order_16ch_testbench is
-end NLC_6th_order_16ch_testbench;
